c07ad6b6fa8682433cc67be74d6ebeff9170ef22
Raghav Nayak <ragsnayak@gmail.com>
2024-05-20 23:20:51 +0530

    Jinja2 template and django apps

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 from . import views
 
@@ -25,4 +25,5 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("about/", views.about, name="about"),
     path("contact/", views.contact, name="contact"),
+    path("chai/", include("chai.urls")),
 ]
