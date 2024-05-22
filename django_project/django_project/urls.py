7735f2ac4c602b10d76df82f8669da625bc0ec55
Raghav Nayak <ragsnayak@gmail.com>
2024-05-22 23:54:21 +0530

    model creation and migration running

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -27,4 +29,4 @@ urlpatterns = [
     path("contact/", views.contact, name="contact"),
     path("chai/", include("chai.urls")),
     path("__reload__", include("django_browser_reload.urls")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
