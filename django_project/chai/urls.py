78cba1c0c52e82779e82b498ff675386a3860932
Raghav Nayak <ragsnayak@gmail.com>
2024-06-02 10:38:26 +0530

    django forms

--- django_project/chai/urls.py
+++ django_project/chai/urls.py
@@ -7,4 +7,5 @@ from . import views
 urlpatterns = [
     path("", views.all_chai, name="all_chai"),
     path("<int:chai_id>/", views.chai_detail, name="chai_detail"),
+    path("chai_stores/", views.chai_store, name="chai_stores"),
 ]
